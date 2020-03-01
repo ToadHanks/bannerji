6198b56e207b09f0578cad8b50079e9b08771405
Mihir Patel <47681434+ToadHanks@users.noreply.github.com>
2020-02-29 23:44:01 -0500

    Add files via upload

--- prototype.R
+++ prototype.R
@@ -1,11 +1,13 @@
-# IN-PROGRESS!!
 library(rjson)
 library(igraph)
 
-
-
 #------------------------------------------------------------------- Data -----------------------------------------
-mytraits <- c("Happy", "Angel", "Curious", "American")
+mytraits <- c("happy", "angel", "curious", "american")
+
+toupper(mytraits)
+
+emoji_json_file <- "https://raw.githubusercontent.com/ToadHanks/urmojis/master/traitslib/mytraits.json" #link
+json_data <- rjson::fromJSON(base::paste(base::readLines(emoji_json_file), collapse = "")) #read line by line make
 
 #------------------------------------------------------------------- Viewer---------------------------------------
 #The diplay/view function wrapped
@@ -16,8 +18,9 @@ displayTraits <<- function(){
   emoji_json_file <- "https://raw.githubusercontent.com/ToadHanks/urmojis/master/traitslib/mytraits.json" #link
   json_data <- rjson::fromJSON(base::paste(base::readLines(emoji_json_file), collapse = "")) #read line by line make
   emo_lib <- base::unlist(base::lapply(json_data,function(x){ x$char })) #unlisted json
-  return(base::data.frame(Emoji= emo_lib))
+  return(base::data.frame(EMOJI= emo_lib))
 }
+displayTraits()
 
 #------------------------------------------------------------ Traits to emojis ------------------------------------------
 
@@ -28,7 +31,7 @@ get_emoji_from_traits <<- function(a_trait) {
   emoji_json_file <- "https://raw.githubusercontent.com/ToadHanks/urmojis/master/traitslib/mytraits.json" #link
   json_data <- rjson::fromJSON(base::paste(base::readLines(emoji_json_file), collapse = "")) #read line by line make
 
-  vector_of_emoji_names_and_characters <- unlist(base::lapply(json_data, function(x){ x$char }))
+  vector_of_emoji_names_and_characters <- base::unlist(base::lapply(json_data, function(x){ x$char }))
 
   emoji_character <- base::unname(vector_of_emoji_names_and_characters[base::names(vector_of_emoji_names_and_characters) == a_trait])
 
@@ -39,28 +42,51 @@ plotMyEmojis <<- function(traits) { # add option for , plotLayout, further custo
   
   #try catch for min length of traits, make it 2 above and it's chracter
   
-  #Add DNA strands
+  dna_strand <- "🧬"
   emoji_bag <- c()
   for(i in seq(traits)){
     emoji_bag <- c(emoji_bag, get_emoji_from_traits(traits[i])) #namespace here is custom function names
   }
-  
+  emoji_bag <- base::rbind(emoji_bag, base::matrix(dna_strand, ncol= base::length(emoji_bag)))
+  emoji_bag <- c(emoji_bag)
   emoji_bag <- base::as.character(base::rev(emoji_bag))
   emo_mat <- base::matrix(emoji_bag, ncol = 2, byrow= T) #add NA for missing values odd pairs!!!
-  links <-  base::data.frame(source= emo_mat[,1], target= emo_mat[,2])
+  links <-  base::data.frame(from = emo_mat[,1], to = emo_mat[,2])
+
   emogg <- igraph::graph_from_data_frame(links, directed = T)
   
   igraph::V(emogg)$size <- 1
-  igraph::V(emogg)$color <- "azure"
+  igraph::V(emogg)$color <- "gray"
   igraph::V(emogg)$frame.color <- "ghostwhite"
   igraph::E(emogg)$arrow.mode <- 0
   isolated <- igraph::degree(igraph::simplify(emogg)) == 0
-  
-
-  igraph::plot.igraph(igraph::delete.vertices(igraph::simplify(emogg), isolated), vertex.label= igraph::V(emogg)$name, asp = 0, layout= igraph::layout_nicely(emogg))
-  
-  #return(emoji_bag) #return the plot??
+  base::setwd("~/Desktop")
+  grDevices::png(filename = "dna.png", width= 1280, height= 720, res= 200)
+  igraph::plot.igraph(igraph::delete.vertices(igraph::simplify(emogg), isolated), vertex.label= igraph::V(emogg)$name, asp= 0, layout= igraph::layout_nicely(emogg))
+  dev.off()
+  base::message("mydna.png is saved to your Desktop!")
 }
-plotMyEmojis(mytraits)
 
-#---------------------------------------------------------------------------------------------------------------------
+#plotMyEmojis(mytraits)
+
+#-------------------------------------------------------- Recommendation ---------------------------------------
+traitsLookup <<- function(){ 
+  #TRY CATCH HERE FOR LINK VERIFICATION!!!
+  emoji_json_file <- "https://raw.githubusercontent.com/ToadHanks/urmojis/master/traitslib/mytraits.json" #link
+  json_data <- rjson::fromJSON(base::paste(base::readLines(emoji_json_file), collapse = "")) #read line by line make
+  
+  
+  {first_key <- base::readline(prompt= "First keyword: ");
+  second_key <- base::readline(prompt= "Second keyword: ");
+  third_key <- base::readline(prompt= "Third keyword: ");
+  fourth_key <- base::readline(prompt= "Fourth keyword: ");}
+  
+  keys_bag <- c(first_key,second_key,third_key,fourth_key)
+  
+  
+  
+  #return() Recommendation
+}
+
+traitsLookup()
+
